8f625369a32fb38d008de52091846c087474e71d
rohan-O7 <rohandasbbsr@gmail.com>
2025-01-03 22:14:05 +0530

    This is the front end part

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+  return (
+    <nav className='bg-black hover:underline-offset-1 text-white py-2 grid place-items-center h-[70px]'>
+
+    <div className='font-bold text-xl mx-9 cursor-pointer'>Task-Manager</div>
+
+    </nav>
+  )
+}
+
+export default Navbar
